7291449812e16b1b0576e6c6bdd718ebe3cb8734
Blank67 <hmnshshrm67@gmail.com>
2022-12-23 15:42:15 +0530

    task 13

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,28 @@
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const count = useSelector((state) => (state.counter));
+  const dispatch = useDispatch();
+
+  const incHandler = () => {
+    dispatch({type: 'inc'});
+  }
+
+  const decHandler = () => {
+    dispatch({type: 'dec'});
+  }
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{count}</div>
+      <div>
+        <button onClick={decHandler}>-5</button>
+        <button onClick={incHandler}>+5</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,11 @@ import {createStore} from 'redux';
 const counterReducer = (state = { counter: 0 }, action) => {
     if (action.type === 'inc') {
         return {
-            counter: state.counter + 1
+            counter: state.counter + 5
         }
     } else if (action.type === 'dec') {
         return {
-            counter: state.counter - 1
+            counter: state.counter - 5
         }
     }
     else {
